08da9f3d94f918a091770a9d46e27017927a8604
Sergey-Kudashev <dossxxx0389@gmail.com>
2025-06-12 18:56:21 +0200

    firebase web

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'app.dart';
+import 'firebase_options.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -12,10 +13,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   timeago.setLocaleMessages('uk', timeago.UkMessages());
   await initializeDateFormatting('uk_UA', null);
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // ❌ Цей рядок можеш прибрати або оновити, якщо потрібна кастомна поведінка
-  FirebaseFirestore.instance.settings = const Settings(persistenceEnabled: true);
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
 
   await FirebaseAppCheck.instance.activate(
     androidProvider: AndroidProvider.debug,
