b16ee40f185f8876267e383732603c4f306acbb5
Sergey-Kudashev <dossxxx0389@gmail.com>
2025-06-15 17:58:51 +0200

    home drawer lag fix v10

--- lib/app.dart
+++ lib/app.dart
@@ -11,14 +11,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'CRM App',
       debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
-      routes: AppRoutes.routes,
-      navigatorObservers: [HeroController()],
-      onGenerateRoute:
-          (settings) => MaterialPageRoute(
-            builder: (_) => const AuthGate(),
-            settings: settings,
-          ),
+      onGenerateInitialRoutes:
+          (_) => [
+            MaterialPageRoute(
+              builder: (_) => const AuthGate(),
+              settings: const RouteSettings(name: '/'),
+            ),
+          ],
+      onGenerateRoute: (settings) {
+        final routeBuilder = AppRoutes.routes[settings.name];
+        if (routeBuilder != null) {
+          return MaterialPageRoute(builder: routeBuilder, settings: settings);
+        }
+        return null;
+      },
     );
   }
 }
