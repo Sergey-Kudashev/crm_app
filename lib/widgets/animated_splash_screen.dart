879c11e15b0e687fd748ac02570251f4dbbcb3e6
Sergey-Kudashev <dossxxx0389@gmail.com>
2025-06-22 22:53:48 +0200

    video in animated splash

--- lib/widgets/animated_splash_screen.dart
+++ lib/widgets/animated_splash_screen.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:html' as html;
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:video_player/video_player.dart';
 
 class AnimatedSplashScreen extends StatefulWidget {
   const AnimatedSplashScreen({super.key});
@@ -13,15 +12,15 @@ class AnimatedSplashScreen extends StatefulWidget {
 }
 
 class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> {
-  late VideoPlayerController _videoController;
-  bool _videoError = false;
+  // bool _gifLoaded = false;
+  bool _gifFailed = false;
 
   @override
   void initState() {
     super.initState();
     _setThemeColorForWeb();
     _preloadHome();
-    _initVideo();
+    _startSplashTimer();
   }
 
   void _setThemeColorForWeb() {
@@ -48,47 +47,12 @@ class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> {
     }
   }
 
-  Future<void> _initVideo() async {
-    try {
-      if (kIsWeb) {
-        _videoController = VideoPlayerController.network('videos/splash.mp4');
-      } else {
-        _videoController = VideoPlayerController.asset('assets/videos/splash.mp4');
+  void _startSplashTimer() {
+    Future.delayed(const Duration(milliseconds: 3200), () {
+      if (mounted) {
+        Navigator.of(context).pushReplacementNamed('/home');
       }
-
-      await _videoController.initialize();
-      _videoController.setLooping(false);
-      _videoController.setVolume(0); // Для Web autoplay
-      _videoController.play();
-
-      // Навігація після завершення
-      _videoController.addListener(() {
-        if (_videoController.value.position >= _videoController.value.duration &&
-            mounted) {
-          Navigator.of(context).pushReplacementNamed('/home');
-        }
-      });
-    } catch (e) {
-      debugPrint('Video init error: $e');
-      setState(() {
-        _videoError = true;
-      });
-
-      // Якщо відео не запускається, перейти через 3 секунди
-      Future.delayed(const Duration(seconds: 3), () {
-        if (mounted) {
-          Navigator.of(context).pushReplacementNamed('/home');
-        }
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    if (!_videoError) {
-      _videoController.dispose();
-    }
-    super.dispose();
+    });
   }
 
   @override
@@ -96,15 +60,28 @@ class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: _videoError || !_videoController.value.isInitialized
+        child: _gifFailed
             ? Image.asset(
                 'assets/splash_logo.png',
                 width: 160,
                 height: 160,
               )
-            : AspectRatio(
-                aspectRatio: _videoController.value.aspectRatio,
-                child: VideoPlayer(_videoController),
+            : Image.asset(
+                'assets/splash.gif',
+                width: 240,
+                height: 240,
+                fit: BoxFit.contain,
+                gaplessPlayback: true,
+                errorBuilder: (context, error, stackTrace) {
+                  setState(() {
+                    _gifFailed = true;
+                  });
+                  return Image.asset(
+                    'assets/splash_logo.png',
+                    width: 160,
+                    height: 160,
+                  );
+                },
               ),
       ),
     );
